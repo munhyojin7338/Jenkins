3fed23ef88c03582b78836d9877753598559c19c
MUN-HyoJin <hyojin7338@naver.com>
2023-04-20 16:08:31 +0900

    Jenkins 연습하기 전 환경세팅하기

--- src/main/java/com/example/jenkins/entity/Member.java
+++ src/main/java/com/example/jenkins/entity/Member.java
@@ -1,4 +1,17 @@
 package com.example.jenkins.entity;
 
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+
 }
